e20d02b7673e5530dc0096ecfdd170ab61c3e195
Subro sutradhar <subroopu@gmail.com>
2026-04-08 09:50:40 +0600

    Implement selection sort algorithm in C++

---
+++ SelectionSort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+void mySwap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        mySwap(arr[i], arr[minIndex]);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
